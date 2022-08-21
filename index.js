b72d8c5a4a2d6e29656564a76140d170139a0eeb
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-08-21 15:20:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(worktodo = "roller-skate") {
+	return `This Saturday, I want to ${worktodo}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+	return `This Monday, I will ${activity}.`;
+}
+let wrapAdjective = function (adjective = "*") {
+	return function (highlight = "a hard worker") {
+		return `You are ${adjective}${highlight}${adjective}!`;
+	};
+};
